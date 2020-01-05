dd702262ec522a61fb81b8fc9636887ee27638c3
Vinicius Escame <v.escame@gmail.com>
2020-01-05 15:57:06 -0300

    implementação das funções básicas do controlador de usuários

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ToDo.Models;
@@ -10,19 +11,84 @@ namespace ToDo.Controllers
     [Route("v1/[controller]")]
     public class UsersController : ControllerBase
     {
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<User>>> GetAllUsers()
-        {
-            return Ok(new User[] { new User() });
-        }
-        [HttpPost]
+        [HttpPost("login")]
         public async Task<ActionResult<User>> LoginUser(User user, [FromServices] IUserService userService)
         {
             var canLogin = await userService.Login(user);
 
-            if (canLogin) return Ok("Login allowed!");
-
+            if (canLogin) 
+                return Ok("Login allowed!");
+            
             return NotFound("Invalid credentials");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetAllUsers([FromServices] IUserService userService)
+        {
+            return Ok(await userService.GetAllUsers());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<User>> GetUserById(int id, [FromServices] IUserService userService)
+        {
+            try
+            {
+                var user = await userService.GetUserById(id);
+                return user;
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> CreateUser([FromBody] User user, [FromServices] IUserService userService)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                return await userService.CreateUser(user);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<User>> UpdateUser(int id,
+            [FromBody] User user, [FromServices] IUserService userService)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (user.Id != id)
+                return BadRequest("ID's doesn't match");
+            
+            try
+            {
+                return await userService.UpdateUser(user);
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<User>> DeleteUser(int id, [FromServices] IUserService userService)
+        {
+            try
+            {
+                return await userService.DeleteUser(id);
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
         }
     }
 }
